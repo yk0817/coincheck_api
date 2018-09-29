f91657d388df4aab782c5764c33670b2cd043ab5
山本　憲 <em072029@gmail.com>
2018-09-29 10:44:18 +0900

    Initial Commit

---
+++ coincheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coincheck
+//
+//  Created by 山本　憲 on 2018/09/29.
+//  Copyright © 2018年 山本　憲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
